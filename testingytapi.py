f0d9c8d35df8cccd435450e6258eb4130715d781
Eric Enström <ericens@kth.se>
2023-11-09 13:41:13 +0100

    added more lol

--- testingytapi.py
+++ testingytapi.py
@@ -1,19 +1,52 @@
 import os
+import pickle
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 
-#Create a flow class with the class method from_client_secrets_file. The method takes client info and scopes and contacts the google api to ask for credentials.
-flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file= "webclientinfo.json", scopes = ['https://www.googleapis.com/auth/youtube.readonly'])
+credentials = None
 
-#Runs a local server which the user is redirected to.
-flow.run_local_server(port=2001, prompt="consent", authorization_prompt_message="u just got scammed lol")
+# Pickle files are files that store python objects as bytestreams (basically just bytes)
+# token.pickle stores the user's credentials from previously successful logins
+if os.path.exists('token.pickle'): #if there exists a token.pickle file
+    print('Loading Credentials From File...') 
+    with open('token.pickle', 'rb') as token: #open the the token.pickle file and reads the bytestream (rb = read bytes) meaning that the token variable is assigned to a stream of the type bytes
+        credentials = pickle.load(token)  #The pickle.load() function deserializes the bytestream contained in token(converts to a python object)
 
-#The returned credentials using the flow class used above.
-credentials = flow.credentials
+# If there are no valid credentials available, then either refresh the token or log in.
+if not credentials or not credentials.valid: #If the credentials varible isn't assigned to anything or the credentials aren't valid:
+    if credentials and credentials.expired and credentials.refresh_token: #if there are credentials and they've expired and there is a refresh token
+        print('Refreshing Access Token...')
+        credentials.refresh(Request()) #request a new access token from the google api using the refresh(request()) method
+    else: #go through the oauth 2.0 to get access tokens and refresh tokens.
+        print('Fetching New Tokens...')
+        flow = InstalledAppFlow.from_client_secrets_file(
+            "webclientinfo.json",
+            scopes=[
+                'https://www.googleapis.com/auth/youtube.readonly'
+            ]
+        )
 
-#Print the returned credentials
-print (credentials.to_json())
+        flow.run_local_server(port=2001, prompt='consent', authorization_prompt_message='u just got scammed lol')
+        credentials = flow.credentials
+
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as f: #opens token.pickle in write byte mode
+            print('Saving Credentials for Future Use...')
+            pickle.dump(credentials, f) #serializes the credentials (turns credentials object into a bytestream) and stores it in the token.pickle file
+
+
+# #Create a flow class with the class method from_client_secrets_file. The method takes client info and scopes and contacts the google api to ask for credentials.
+# flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file= "webclientinfo.json", scopes = ['https://www.googleapis.com/auth/youtube.readonly'])
+
+# #Runs a local server which the user is redirected to.
+# flow.run_local_server(port=2001, prompt="consent", authorization_prompt_message="u just got scammed lol")
+
+# #The returned credentials using the flow class used above.
+# credentials = flow.credentials
+
+# #Print the returned credentials
+# print (credentials.to_json())
 
 #Build a service object that is needed to make requests of google API
 service = build("youtube", "v3", credentials = credentials)
@@ -31,6 +64,8 @@ response = request.execute()
 #Since reponse is a dictionary we can use the get() function to get all the values (in the form of a list) corresponding to the key "items". These values will themselves be dictionaries containing info about individual playlists for the account.
 playlist_list = response.get("items", [])
 
+itemId = []
+
 #For loop for printing the individual playlist titles
 for playlist in playlist_list:
   #In reponse => items (list of playlists)
@@ -38,20 +73,34 @@ for playlist in playlist_list:
   #In snippets => title (title of playlist key)
   playlist_title = playlist.get("snippet", {}).get("title")
   playlist_id = playlist.get("id")
-  if playlist_title == "Walk thru wall":
-    itemId = playlist_id; 
+  # print(f"Title: {playlist_title}")
+  saved_pl = ["Summer", "Plugg spellista", "UK Bangerz", "Vibe mf", "Life", "Walk thru wall", "Wesley the 🐐", "Movie", "Oh mah gawd", "Forgotten bangerz", "25 låtar", "Slow times", "G(old)", "Workout", "Bangers"]
+  if playlist_title in saved_pl:
+    itemId.append(playlist_id)
     print(f"Title: {playlist_title} \nID: {playlist_id}")
 
 
-request2 = service.playlistItems().list(
-  part="snippet",
-  playlistId = itemId
-  )
+print("")
 
-response2 = request2.execute()
 
-playlist_songs = response2.get("items",[])
-  
-for song in playlist_songs:
-  song_title = song.get("snippet", {}).get("title")
-  print(song_title)
+for i in range(0, len(itemId)):
+  print(f"\n \n")
+  request2 = service.playlistItems().list(
+    part="snippet",
+    playlistId = itemId[i],
+    maxResults = 50
+    )
+
+  response2 = request2.execute()
+
+  playlist_songs = response2.get("items",[])
+
+  for song in playlist_songs:
+    song_title = song.get("snippet", {}).get("title")
+    song_id = song.get("id")
+    song_artist = song.get("snippet").get("videoOwnerChannelTitle")
+    print(f"Song: {song_title} \nArtist: {song_artist} \nID: {song_id}")
+
+
+
+
